00ccd865b9aad0a632e11b548464a4df438a839c
nirojuderavi <nirojuderavi@outlook.com>
2023-10-26 12:35:07 -0300

    WIP - Adding more styling

--- client-app/src/app/layout/MenuBar.tsx
+++ client-app/src/app/layout/MenuBar.tsx
@@ -1,5 +1,4 @@
 import { Icon, Menu } from "semantic-ui-react";
-import "./styles.css";
 import { NavLink, useLocation } from "react-router-dom";
 
 export default function MenuBar()
@@ -7,26 +6,16 @@ export default function MenuBar()
   console.log(useLocation());
 
   return (
-    <Menu icon="labeled">
+    <Menu className="menuHeader" inverted pointing secondary icon="labeled">
       <Menu.Item as={NavLink} to="" header>
         <Icon name="ils" />
         Niro's Bakery
       </Menu.Item>
-      <Menu.Item
-        as={NavLink}
-        index={1}
-        to="/nonveg"
-        name="Veg Savoury Items"
-      >
+      <Menu.Item as={NavLink} index={1} to="/nonveg" name="Veg Savoury Items">
         <Icon name="leaf" />
         Veg Savoury Items
       </Menu.Item>
-      <Menu.Item
-        as={NavLink}
-        index={2}
-        to="/veg"
-        name="Meat Savoury Items"
-      >
+      <Menu.Item as={NavLink} index={2} to="/veg" name="Meat Savoury Items">
         <Icon name="sticker mule" />
         Meat Savoury Items
       </Menu.Item>
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,5 +1,37 @@
-import { Container } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Button, Container, Header, Icon } from "semantic-ui-react";
+
 
 export default function HomePage() {
-  return <Container><h1>Welcome to Niro's Bakery</h1></Container>;
+  return (
+    <Container
+      textAlign="center"
+      style={{
+        paddingTop:"10%"
+      }}
+    >
+      <Header
+        as="h1"
+        style={{
+          color: "white",
+        }}
+        icon
+      >
+        <Icon name="food" />
+        Niro's Nibbles
+        <Header.Subheader style={{ color: "white" }}>
+          Spice in the right place
+        </Header.Subheader>
+        <Button
+          style={{ marginTop: "10%" }}
+          as={Link}
+          to={"/veg"}
+          primary
+          size="huge"
+        >
+          Feed Me
+        </Button>
+      </Header>
+    </Container>
+  );
 }
